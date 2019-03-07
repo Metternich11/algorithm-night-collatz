dfc40b176ec884ad4fb17e96a42bb86d1e45eb07
Alan Hu <on.alanhu@gmail.com>
2019-03-07 09:49:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 function collatz (n) {
-  const chain = [];
-  while (n !== 1) {
-    chain.push(n);
-    n = (n % 2 === 0) ? (n / 2) : (3 * n + 1);
-  }
-  chain.push(n);
-  return chain;
+  // Your solution here
 }
 
 function longestBetween (i) {
-  let longest = null;
-  let longestLength = 0;
-  for (let n = 1; n < i; n++) {
-    const currentLength = collatz(n).length;
-    if (longestLength < currentLength) {
-      longest = n;
-      longestLength = currentLength;
-    }
-  }
-  return longest;
+  // Your solution here
 }
-
-console.log(longestBetween(1, 10 ** 8));
 
 module.exports = { collatz, longestBetween };
