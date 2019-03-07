32e8531d9390dae0f0eca41a2b97b346fb6403ad
Rodion Raskolnikov <RR@Oles-MBP.lan>
2019-03-07 19:50:24 +0100

    first2

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+
 function collatz (n) {
-  // Your solution here
+  const res = [];
+  while (n !== 1) {
+    res.push(n);
+    if (n%2 === 0) n = n/2;
+    else n = 3*n+1;
+  }
+  res.push(1);
+  return res;
 }
 
-function longestBetween (i) {
-  // Your solution here
+function longestBetween (n) {
+  let max = 0;
+  let maxNo;
+  let arr =[];
+  for (let i = 1; i <= n; i++) {
+    arr = collatz(i);
+    if (arr.length > max) {
+      maxNo = i;
+      max = arr.length;
+    }
+  }
+  return maxNo;
 }
 
 module.exports = { collatz, longestBetween };
